80ba352d206993efd79735e0ce3377e09145b71a
Daniel Helmersson <daniel@mobynet.se>
2026-02-17 18:20:20 +0100

    fixed port checking

--- quick-start.sh
+++ quick-start.sh
@@ -157,7 +157,7 @@ find_available_port() {
         port=$((port + 1))
     done
     ALLOCATED_PORTS="$ALLOCATED_PORTS $port"
-    echo "$port"
+    FOUND_PORT=$port
 }
 
 # Find a contiguous block of $2 available ports starting from $1
@@ -177,7 +177,7 @@ find_available_port_range() {
             for ((p=start; p<start+count; p++)); do
                 ALLOCATED_PORTS="$ALLOCATED_PORTS $p"
             done
-            echo "$start"
+            FOUND_PORT=$start
             return
         fi
     done
@@ -192,13 +192,13 @@ DEFAULT_PORT_OPCUA_START=4840
 DEFAULT_PORT_MODBUS=502
 OPCUA_COUNT=41  # Up to 41 ports for dynamic line selection (4840-4880)
 
-PORT_NGINX=$(find_available_port $DEFAULT_PORT_NGINX)
-PORT_GRAFANA=$(find_available_port $DEFAULT_PORT_GRAFANA)
-PORT_PGBOUNCER=$(find_available_port $DEFAULT_PORT_PGBOUNCER)
-PORT_SIMULATOR=$(find_available_port $DEFAULT_PORT_SIMULATOR)
-PORT_UMH=$(find_available_port $DEFAULT_PORT_UMH)
-PORT_MODBUS=$(find_available_port $DEFAULT_PORT_MODBUS)
-PORT_OPCUA_START=$(find_available_port_range $DEFAULT_PORT_OPCUA_START $OPCUA_COUNT)
+find_available_port $DEFAULT_PORT_NGINX;    PORT_NGINX=$FOUND_PORT
+find_available_port $DEFAULT_PORT_GRAFANA;  PORT_GRAFANA=$FOUND_PORT
+find_available_port $DEFAULT_PORT_PGBOUNCER; PORT_PGBOUNCER=$FOUND_PORT
+find_available_port $DEFAULT_PORT_SIMULATOR; PORT_SIMULATOR=$FOUND_PORT
+find_available_port $DEFAULT_PORT_UMH;      PORT_UMH=$FOUND_PORT
+find_available_port $DEFAULT_PORT_MODBUS;   PORT_MODBUS=$FOUND_PORT
+find_available_port_range $DEFAULT_PORT_OPCUA_START $OPCUA_COUNT; PORT_OPCUA_START=$FOUND_PORT
 OPCUA_END=$((PORT_OPCUA_START + OPCUA_COUNT - 1))
 
 # Check for conflicts
